b86fb226be06213e38b84629e4f6d5f3ea80da8d
realbxnnie <bonnieroxtigers@yandex.ru>
2025-09-15 09:05:20 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -1,34 +1,5 @@
 #!/usr/bin/env bash
-
-UTIL_DIR="$HOME/.local/share/emperorutils"
-mkdir -p "$UTIL_DIR"
-
-git clone https://github.com/emperoros/emperorutils "$UTIL_DIR/emperorutils"
-cd "$UTIL_DIR/emperorutils" || exit
-
 for utility in ./src/*; do
-    name=$(basename "$utility")
-    cp "$utility" "$UTIL_DIR/$name"
-
-    # Bash/Zsh
-    for shellrc in "$HOME/.bashrc" "$HOME/.zshrc"; do
-        if ! grep -Fxq "source $UTIL_DIR/$name" "$shellrc"; then
-            echo "source $UTIL_DIR/$name" >> "$shellrc"
-        fi
-    done
-
-    # Fish
-    FISH_CONFIG="$HOME/.config/fish/config.fish"
-    mkdir -p "$(dirname "$FISH_CONFIG")"
-    if ! grep -Fxq "source $UTIL_DIR/$name" "$FISH_CONFIG"; then
-        echo "source $UTIL_DIR/$name" >> "$FISH_CONFIG"
-    fi
-
-    # Csh/Tcsh
-    for shellrc in "$HOME/.cshrc" "$HOME/.tcshrc"; do
-        if [ -f "$shellrc" ] && ! grep -Fxq "source $UTIL_DIR/$name" "$shellrc"; then
-            echo "source $UTIL_DIR/$name" >> "$shellrc"
-        fi
-    done
-
+    chmod +x "$utility"
+    sudo cp "$utility" "/usr/local/bin/$(basename "$utility")"
 done
